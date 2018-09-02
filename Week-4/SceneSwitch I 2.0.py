2b4ccfe9535f27ff31af7164d45ba464b799c5e5
max180643 <max180643@gmail.com>
2018-09-02 15:13:44 +0700

    Update code

--- Week-4/SceneSwitch I 2.0.py
+++ Week-4/SceneSwitch I 2.0.py
@@ -2,20 +2,31 @@
 SceneSwitch I
 Author : Chanwit Settavongsin
 """
-def main(text):
-    """Warmlight Count"""
-    light = 1 # 0 = off, 1 = on 
-    mode = 0 # 0 = daylight, 1 = warmlight
-    warm = 0 # answer 
-    temp = 0 # old time
-    while text != "End":
-        time = input()
-        if light != 0:
-            light = 0
-            temp = time
-        elif time - temp <= 6:
-            
+def main():
+    """ Warmlight Count """
+    now_status = 1 # 0 = off, 1 = daylight, 2 = warmlight
+    last_status = 1 # 1 = daylight, 2 = warmlight
+    warm = 0 # answer
+    start_time = input()
+    if start_time != "End":
+        while True:
+            last_time = input()
+            if last_time == "End":
+                break
+            elif now_status != 0:
+                now_status = 0
+                start_time = last_time
+            elif float(last_time) - float(start_time) <= 6:
+                if last_status == 1:
+                    now_status = 2
+                else:
+                    now_status = 1
+                last_status = now_status
+                if now_status == 2:
+                    warm += 1
+            else:
+                now_status = 1
+                last_status = 1
+    print(warm)
 
-        
-
-main(input())
+main()
